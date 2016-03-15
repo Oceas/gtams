3f1b4040ae762b2002a1327372a5c900fe0425b0
scott <scott@getcodesmart.com>
2016-03-15 14:15:42 -0400

    First Commit

---
+++ forms/signup.php
@@ -0,0 +1,34 @@
+
+
+<div class="row padding10">
+    <form class="col s12" autocomplete="nope">
+        <div class="row">
+            <div class="input-field col s6">
+                <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                <label for="first_name">First Name</label>
+            </div>
+            <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" autocomplete="off">
+                <label for="last_name">Last Name</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="email" type="email" class="validate">
+                <label for="email">Email</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">Password</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">Confirm Password</label>
+            </div>
+        </div>
+    </form>
+</div>
