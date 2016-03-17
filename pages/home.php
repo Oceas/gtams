cc166623bc00c76ca33fc19a8978978ad38f12b5
scott <scott@getcodesmart.com>
2016-03-17 07:31:11 -0400

    Rearranged files to better support templating

---
+++ pages/home.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<div class="row">
+    <div class="col s12">
+        <div class="center card-panel teal">
+          <span class="white-text">
+              TODO: Home welcome text About GTASS
+          </span>
+        </div>
+    </div>
+</div>
---
+++ template.php
@@ -0,0 +1,3 @@
+<!--Header-->
+<?php require "header.php"; ?>
+
