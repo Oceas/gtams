142c6ece59b54a1083ed1884496893d4cc3b520c
Scott Anderson <scott@getcodesmart.com>
2016-03-29 23:09:54 -0400

    Completed login form. Added first half of register form fields

--- forms/login-form.php
+++ forms/login-form.php
@@ -1,23 +1,18 @@
 <form>
     <div class="row">
-        <div class="six columns">
-            <label for="exampleEmailInput">Your email</label>
-            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
-        </div>
-        <div class="six columns">
-            <label for="exampleRecipientInput">Reason for contacting</label>
-            <select class="u-full-width" id="exampleRecipientInput">
-                <option value="Option 1">Questions</option>
-                <option value="Option 2">Admiration</option>
-                <option value="Option 3">Can I get your number?</option>
-            </select>
+        <div class="twelve columns text-center">
+            <h3>Login Screen</h3>
         </div>
     </div>
-    <label for="exampleMessage">Message</label>
-    <textarea class="u-full-width" placeholder="Hi Dave …" id="exampleMessage"></textarea>
-    <label class="example-send-yourself-copy">
-        <input type="checkbox">
-        <span class="label-body">Send a copy to yourself</span>
-    </label>
+    <div class="row">
+        <div class="six columns">
+            <label for="email">Email</label>
+            <input class="u-full-width" type="text" placeholder="example@knights.ucf.edu" id="email">
+        </div>
+        <div class="six columns">
+            <label for="password">Password</label>
+            <input class="u-full-width" type="password" id="password">
+        </div>
+    </div>
     <input class="button-primary" type="submit" value="Submit">
 </form>
--- forms/register-form.php
+++ forms/register-form.php
@@ -1,26 +1,73 @@
 <form>
     <div class="row">
+        <div class="twelve columns text-center">
+            <h3>GTA Registration Form</h3>
+        </div>
+    </div>
+    <div class="row">
         <div class="six columns">
-            <label for="exampleEmailInput">Your email</label>
-            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+            <label for="firstName">First Name</label>
+            <input class="u-full-width" type="text" placeholder="John" id="firstName">
         </div>
         <div class="six columns">
-            <label for="exampleRecipientInput">Major</label>
-            <select class="u-full-width" id="exampleRecipientInput">
-                <option value="Option 1">Questions</option>
-                <option value="Option 2">Admiration</option>
-                <option value="Option 3">Can I get your number?</option>
+            <label for="lastName">Last Name</label>
+            <input class="u-full-width" type="text" placeholder="Doe" id="lastName">
+        </div>
+    </div>
+    <div class="row">
+        <div class="six columns">
+            <label for="email">Knights Email</label>
+            <input class="u-full-width" type="email" placeholder="example@knights.ucf.edu" id="email">
+        </div>
+        <div class="six columns">
+            <label for="ucfid">UCFID: (formerly PID:no letter)</label>
+            <input class="u-full-width" type="text" placeholder="1324567" id="ucfid">
+        </div>
+    </div>
+    <div class="row">
+        <div class="six columns">
+            <label for="password">Password</label>
+            <input class="u-full-width" type="password" id="password">
+        </div>
+        <div class="six columns">
+            <label for="confirmPassword">Confirm Password</label>
+            <input class="u-full-width" type="password" id="confirmPassword">
+        </div>
+    </div>
+    <div class="row">
+        <div class="six columns">
+            <label for="phoneNumber">Phone Number</label>
+            <input class="u-full-width" type="tel" placeholder="123-456-7890" id="phoneNumber">
+        </div>
+        <div class="six columns">
+            <label for="csStudent">Are you a PH.D. Student in Computer Science?</label>
+            <div style="padding:10px;">
+                <input type="radio" name="csStudent" value="yes" checked> Yes </input>
+                <input type="radio" name="csStudent" style="margin-left: 20px;" value="no"> No </input>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="six columns">
+            <label for="semestersGS">Number of semester (including summers) working as a graduate student</label>
+            <input class="u-full-width" type="text" placeholder="0" id="semestersGS">
+        </div>
+        <div class="six columns">
+            <label for="speakTest">Have you passed the SPEAK Test?</label>
+            <select class="u-full-width" id="speakTest">
+                <option value="null">Please select one</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+                <option value="graduateUSA">Graduated from a U.S. Institution</option>
+                <option value="newlyAdmitted">Newly Admitted Student</option>
             </select>
         </div>
     </div>
     <div class="row">
         <div class="six columns">
-            <label for="password">New Password</label>
-            <input class="u-full-width" type="password" id="password" name="password">
-        </div>
-        <div class="six columns">
-            <label for="confirmPassword">Confirm Password</label>
-            <input class="u-full-width" type="password" id="confirmPassword" name="confirmPassword">
+            <label for="semestersGTA">Number of semesters (including summers) working as a graduate teaching assistant</label>
+            <input class="u-full-width" type="text" placeholder="0" id="semestersGTA">
         </div>
     </div>
     <input class="button-primary" type="submit" value="Submit">
