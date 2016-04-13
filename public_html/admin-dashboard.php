e0f53d22dc8b87c3d6219de213942a33c62aa4c4
Brandon Aulet <brandonaulet500@gmail.com>
2016-04-13 04:38:38 -0400

    Fixed issues with merge.

--- public_html/admin-dashboard.php
+++ public_html/admin-dashboard.php
@@ -3,87 +3,6 @@
   require "header.php";
 ?>
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<form>
-    <div class="row">
-        <div class="twelve columns text-center">
-            <h3>Create New Application Session</h3>
-        </div>
-        <div class="row">
-            <div class="twelve columns">
-                <label>Name of Application Session</label>
-                <input class="u-full-width" type="text" name="sessionName"/>
-            </div>
-        </div>
-        <div class="row">
-            <div class="six columns">
-                <label>Application Submission Deadline</label>
-                <input class="u-full-width" type="date" name="submissionDeadlineDate">
-            </div>
-            <div class="six columns">
-                <label>Academic Advisor Letter Deadline</label>
-                <input class="u-full-width" type="date" name="letterDeadlineDate">
-            </div>
-        </div>
-        <div class="row">
-            <div class="row">
-                <div class="six columns">
-                    <label>Name of the GTA Chair</label>
-                </div>
-                <div class="three columns">
-                    <label>Email</label>
-                </div>
-                <div class="three columns">
-                    <label>Password</label>
-                </div>
-            </div>
-            <div class="dynamic-list">
-                <div class="six columns">
-                    <input class="u-full-width" placeholder="Dr. Jane Doe" type="text"
-                           name="gcChairName">
-                </div>
-                <div class="three columns">
-                    <input class="u-full-width" type="email" placeholder="example@ucf.edu" name="gcChairEmail">
-                </div>
-                <div class="three columns">
-                    <input class="u-full-width" type="password" name="gcChairPassword">
-                </div>
-
-            </div>
-        </div>
-        <div class="row">
-            <div class="row">
-                <div class="six columns">
-                    <label>Name of Other GC Members</label>
-                </div>
-                <div class="three columns">
-                    <label>Email</label>
-                </div>
-                <div class="three columns">
-                    <label>Password</label>
-                </div>
-            </div>
-            <div id="dynamic-gcmembers" class="row">
-                <div class="six columns">
-                    <input class="u-full-width" placeholder="Dr. Jane Doe" type="text"
-                           name="gcMembers[name][]">
-                </div>
-                <div class="three columns">
-                    <input class="u-full-width" type="email" placeholder="example@ucf.edu" name="gcMembers[email][]">
-                </div>
-                <div class="three columns">
-                    <input class="u-full-width" type="password" name="gcMembers[password][]">
-                </div>
-            </div>
-            <div class="row">
-                <div class="three columns">
-                    <input class="u-full-width button-primary" type="button" onclick="addGCMember('dynamic-gcmembers');" value="Add Another GC Member">
-                </div>
-            </div>
-        </div>
-        <input class="button-primary" type="submit" value="Submit">
-=======
 <form action="ad-dash_connect.php" method="post">
   <div class="row">
      <div class="twelve columns text-center">
@@ -129,53 +48,6 @@
                  <input class="u-full-width" type="password" name="gcChairPassword">
              </div>
 
-=======
-<form action="ad-dash_connect.php" method="post">
-  <div class="row">
-     <div class="twelve columns text-center">
-         <h3>Create New Application Session</h3>
-     </div>
-     <div class="row">
-         <div class="twelve columns">
-             <label>Name of Application Session</label>
-             <input class="u-full-width" type="text" name="sessionName"/>
-         </div>
-     </div>
-     <div class="row">
-         <div class="six columns">
-             <label>Application Submission Deadline</label>
-             <input class="u-full-width" type="date" name="submissionDeadlineDate">
-         </div>
-         <div class="six columns">
-             <label>Academic Advisor Letter Deadline</label>
-             <input class="u-full-width" type="date" name="letterDeadlineDate">
-         </div>
-     </div>
-     <div class="row">
-         <div class="row">
-             <div class="six columns">
-                 <label>Name of the GTA Chair</label>
-             </div>
-             <div class="three columns">
-                 <label>Email</label>
-             </div>
-             <div class="three columns">
-                 <label>Password</label>
-             </div>
-         </div>
-         <div class="dynamic-list">
-             <div class="six columns">
-                 <input class="u-full-width" placeholder="Dr. Jane Doe" type="text"
-                        name="gcChairName">
-             </div>
-             <div class="three columns">
-                 <input class="u-full-width" type="email" placeholder="example@ucf.edu" name="gcChairEmail">
-             </div>
-             <div class="three columns">
-                 <input class="u-full-width" type="password" name="gcChairPassword">
-             </div>
-
->>>>>>> Stashed changes
          </div>
      </div>
      <div class="row">
@@ -209,10 +81,6 @@
          </div>
      </div>
      <input class="button-primary" type="submit" value="Submit">
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 </form>
 
 <script>
