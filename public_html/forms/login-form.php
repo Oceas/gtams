3235591015426d839711930596797f370a912613
Brandon Aulet <brandonaulet500@gmail.com>
2016-04-13 04:28:04 -0400

    Made dbopen easier to edit. Login page working.

--- public_html/forms/login-form.php
+++ public_html/forms/login-form.php
@@ -7,11 +7,11 @@
     <div class="row">
         <div class="six columns">
             <label for="email">Email</label>
-            <input class="u-full-width" type="text" placeholder="example@knights.ucf.edu" name="email">
+            <input class="u-full-width" type="text" placeholder="example@knights.ucf.edu" id="email">
         </div>
         <div class="six columns">
             <label for="password">Password</label>
-            <input class="u-full-width" type="password" name="password">
+            <input class="u-full-width" type="password" id="password">
         </div>
     </div>
     <input class="button-primary" type="submit" value="Submit">
