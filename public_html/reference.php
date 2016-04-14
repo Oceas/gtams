a1b1504f7df848c16504d1b9968eeb0f7dc5a38b
T. Brandon Ashley <tbash@knights.ucf.edu>
2016-04-13 21:11:28 -0400

    Updates db with bool for warning sent

--- public_html/reference.php
+++ public_html/reference.php
@@ -2,19 +2,13 @@
    require "header.php";
    if (!empty($_POST)) {
       //Variable from advisor form
-      $sessID = $_GET["sessionId"];
+      $sessionId = $_GET["sessionId"];
       $pid = $_GET["pid"];
       $letterText = $_POST["advisementLetterTyped"];
       var_export($letterText);
-
-      $sql = "UPDATE applicants SET letter=".$letterText." WHERE pid=".$pid." AND semester_session_id=".$sessionId;
-      $sht->query(sql);
-
-
-
-
-
-
+      $sql = "UPDATE applicants SET letter='".$letterText."' WHERE pid='".$pid."' AND semester_session_id='".$sessionId."';";
+      var_dump($sql);
+      $sht->query($sql);
    }
 
 ?>
