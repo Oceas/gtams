499f16dbd09a3a06b894ff2fe3dd73db568ffe56
Josh <farvaallmighty@gmail.com>
2016-04-14 06:39:00 -0400

    update advisor reference

--- public_html/reference.php
+++ public_html/reference.php
@@ -1,16 +1,9 @@
 <?php
-   //require "header.php";
-   if (!empty($_POST)) {
-      //Variable from advisor form
-      $sessId = $_GET["sessionId"];
-      echo $sessId;
-      $pid = $_GET["pid"];
-      $letterText = $_POST["advisementLetterTyped"];
-
-      $sql = "UPDATE applicants SET letter='$letterText' WHERE pid='$pid' AND semester_session_id='$sessId'";
-      var_dump($sql);
-      //die;
-      $dbh->query($sql);
-   }
-
+   require "header.php";
+?>
+<?php
+   require "forms/advisor-form.php";
+?>
+<?php
+   require "footer.php";
 ?>
